ddd9b9b74bb9ab79b81742643f557e9f652e7d7e
Stokkelol <redwhite@bigmir.net>
2016-05-27 14:08:30 +0300

    Video

--- resources/views/frontend/videos/index.blade.php
+++ resources/views/frontend/videos/index.blade.php
@@ -2,17 +2,21 @@
 
 @section('content')
     <div class="container main-video">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            @foreach ($videos as $video)
-                <span class="video-title">{{ $video->title }}</span>
-                {{ $video->excerpt }}
-                <div class="video-main">
-                    {{ $video->video }}
-                </div>
-                <div  class="video-author clearfix">
-                    <em> {{ $video->published_at->diffForHumans() }} </em>
-                </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                @foreach ($videos as $video)
+                    <span class="video-title">{{ $video->title }}</span>
+                        {{ $video->excerpt }}
+                    <div class="video-main">
+                        {{ $video->video }}
+                    </div>
+                    <div  class="video-author clearfix">
+                        <em> {{ $video->published_at->diffForHumans() }} </em>
+                    </div>
+                    <hr>
+                @endforeach
+            </div>
         </div>
     </div>
-            @endforeach
+
 @endsection
