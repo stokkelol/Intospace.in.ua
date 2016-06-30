f77606ebcb4c9371b58e2fdd033130d93eef76f5
Stokkelol <redwhite@bigmir.net>
2016-06-30 21:04:57 +0300

    v2

--- resources/views/frontend/videos/video.blade.php
+++ resources/views/frontend/videos/video.blade.php
@@ -14,7 +14,7 @@
                   <em>{{ $video->published_at->diffForHumans() }}</strong></em>
               </div>
               <br>
-              <p>{{ $video->excerpt }}</p>
+              <p>{!! $video->excerpt !!}</p>
               </div>
           </div>
           <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
