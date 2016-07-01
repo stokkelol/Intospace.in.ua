1e0ef924d7f734c63aa23d003c96e247a3cc1b7b
Stokkelol <redwhite@bigmir.net>
2016-07-01 17:54:34 +0300

    bands index page

--- app/Post.php
+++ app/Post.php
@@ -7,10 +7,12 @@ use AlgoliaSearch\Laravel\AlgoliaEloquentTrait;
 use Cviebrock\EloquentSluggable\SluggableInterface;
 use Cviebrock\EloquentSluggable\SluggableTrait;
 use Cache;
+use App\Traits\FiltersTrait;
 
 class Post extends Entity implements SluggableInterface
 {
     use SluggableTrait;
+    use FiltersTrait;
 
     protected $table = 'posts';
 
