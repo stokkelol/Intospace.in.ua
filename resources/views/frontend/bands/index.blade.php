5fdbcdc1b0a6f306aaea2740d0d072acc3fb92d8
Stokkelol <redwhite@bigmir.net>
2016-07-04 12:11:50 +0300

    active

--- resources/views/frontend/bands/index.blade.php
+++ resources/views/frontend/bands/index.blade.php
@@ -5,22 +5,24 @@
         <div class="row bands-container">
             <ul class="list-unstyled">
                 @foreach($bands as $band)
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 band-container">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            <p class="band-title pull-right">{{ $band->title }}</p>
+                    @if($band->status == 'active')
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 band-container">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                <p class="band-title pull-right">{{ $band->title }}</p>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                @foreach($band->posts as $post)
+                                    <li><p><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></p></li>
+                                @endforeach
+                                @foreach($band->videos as $video)
+                                    <li><p><a href="/videos/{{ $video->slug }}">{{ $video->title }}</a></p></li>
+                                @endforeach
+                                @foreach($band->reviews as $review)
+                                    <li><p><a href="/reviews/{{ $review->slug }}">{{ $review->title }}</a></p></li>
+                                @endforeach
+                            </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                            @foreach($band->posts as $post)
-                                <li><p><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></p></li>
-                            @endforeach
-                            @foreach($band->videos as $video)
-                                <li><p><a href="/videos/{{ $video->slug }}">{{ $video->title }}</a></p></li>
-                            @endforeach
-                            @foreach($band->reviews as $review)
-                                <li><p><a href="/reviews/{{ $review->slug }}">{{ $review->title }}</a></p></li>
-                            @endforeach
-                        </div>
-                    </div>
+                    @endif
                 @endforeach
             </ul>
         </div>
