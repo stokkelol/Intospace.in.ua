bc97ecaba53cf75d09f4a027fc66f29925a25e5f
Stokkelol <redwhite@bigmir.net>
2016-07-29 15:50:40 +0300

    review

--- app/ViewComposers/MonthlyReviewComposer.php
+++ app/ViewComposers/MonthlyReviewComposer.php
@@ -4,14 +4,22 @@ namespace App\ViewComposers;
 
 use Illuminate\Contracts\View\View;
 use App\Repositories\MonthlyReviewRepository;
+use App\Repositories\PostRepository;
+use App\Repositories\VideoRepository;
 
 class MonthlyReviewComposer
 {
     protected $review;
+    protected $post;
+    protected $video;
 
-    public function __construct(MonthlyReviewRepository $review)
+    public function __construct(MonthlyReviewRepository $review,
+                                PostRepository $post,
+                                VideoRepository $video)
     {
         $this->review = $review;
+        $this->post = $post;
+        $this->video = $video;
     }
 
     /**
@@ -19,8 +27,13 @@ class MonthlyReviewComposer
      */
     public function compose(View $view)
     {
-        $review = $this->review->getActiveReview()->first();
+        $review        = $this->review->getActiveReview()->first();
+        $latest_posts  = $this->post->getPostsById($review->latest_posts);
+        $popular_posts = $this->post->getPostsById($review->popular_posts);
+        $latest_videos = $this->video->getVideosById($review->latest_videos);
 
-        $view->with('review', $review);
+        $view->with('review', $review)->with('latest_posts', $latest_posts)
+            ->with('popular_posts', $popular_posts)
+            ->with('latest_videos', $latest_videos);
     }
 }
--- resources/views/frontend/partials/review.blade.php
+++ resources/views/frontend/partials/review.blade.php
@@ -7,25 +7,31 @@
                     <a href="" class="review-title">{{ $review->title }}</a>
                 </div>
                 <div class="col-lg-10 col-lg-offset-1">
-
+                    {{ $review->content }}
                 </div>
                 <div class="review-links">
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <p>Обзоры за прошедший месяц:</p>
-                        <ul class="list-unstyled">
-
+                        <ul class="list-unstyled cl-effect-1 review-items">
+                            @foreach($latest_posts as $post)
+                                <li><a href="{{ $post->slug }}">{{ $post->title }}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <p>Видео за прошедший месяц:</p>
-                        <ul class="list-unstyled">
-
+                        <ul class="list-unstyled cl-effect-1 review-items">
+                            @foreach($latest_videos as $video)
+                                <li><a href="{{ $video->slug }}">{{ $video->title }}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                         <p>Популярные обзоры:</p>
-                        <ul class="list-unstyled">
-
+                        <ul class="list-unstyled cl-effect-1 review-items">
+                            @foreach($popular_posts as $post)
+                                <li><a href="{{ $post->slug }}">{{ $post->title }}</a></li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
