ab615afe23313dc1784fba996d0ab21d4d28a3cb
Stokkelol <redwhite@bigmir.net>
2016-08-02 19:47:10 +0300

    strip tags for share buttons

--- resources/views/frontend/partials/meta.blade.php
+++ resources/views/frontend/partials/meta.blade.php
@@ -9,17 +9,17 @@
     <meta property="og:image" content="http://www.intospace.in.ua/upload/images/intospace.jpg" />
 @else
     <title>{{ isset($title) ? $title : 'Intospace' }}</title>
-    <meta name="description" content="{{ isset($post->excerpt) ? $post->excerpt : '' }}">
+    <meta name="description" content="{{ isset($post->excerpt) ? strip_tags($post->excerpt) : '' }}">
     <meta property="og:url" content="http://www.intospace.in.ua/{{ isset($post->slug) ? $post->slug : ''}}"/>
     <meta property="og:type" content="article" />
-    <meta property="og:description" content="{{ isset($post->excerpt) ? $post->excerpt : 'intospace.in.ua - мрак и блог, вот это вот' }}" />
+    <meta property="og:description" content="{{ isset($post->excerpt) ? strip_tags($post->excerpt) : 'intospace.in.ua - мрак и блог, вот это вот' }}" />
     <meta property="og:title" content="{{ isset($post->title) ? $post->title : 'Intospace.in.ua' }}" />
     <meta property="og:image" content="{{ isset($post->img) ? 'http://www.intospace.in.ua/upload/covers/'.$post->img : '' }}" />
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@intospace.in.ua">
     <meta name="twitter:creator" content="@redwhite">
     <meta name="twitter:title" content="{{ isset($post->title) ? $post->title : 'Intospace' }}">
-    <meta name="twitter:description" content="{{ isset($post->excerpt) ? $post->excerpt : 'intospace.in.ua - мрак и блог, вот это вот' }}">
+    <meta name="twitter:description" content="{{ isset($post->excerpt) ? strip_tags($post->excerpt) : 'intospace.in.ua - мрак и блог, вот это вот' }}">
     <meta name="twitter:image" content="http://www.intospace.in.ua/upload/covers/{{ isset($post->img) ? $post->img : '' }}">
 @endif
     <meta property="og:locale" content="ru_RU" />
