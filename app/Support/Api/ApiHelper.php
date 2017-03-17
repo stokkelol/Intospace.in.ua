955283d22ee102035cace253bab5ff67318232a3
Stokkelol <redwhite@bigmir.net>
2017-03-17 16:43:19 +0200

    go

---
+++ app/Support/Api/ApiHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ion
+ * Date: 15.03.17
+ * Time: 19:23
+ */
+
+namespace app\Support\Api;
+
+
+class ApiHelper
+{
+
+}
---
+++ app/Support/Logger/Logger.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ion
+ * Date: 15.03.17
+ * Time: 19:24
+ */
+
+namespace app\Support\Logger;
+
+
+class Logger
+{
+
+}
