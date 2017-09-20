9cce33193f31aef6bf5c9172719543fe0666b4b7
Stokkelol <redwhite@bigmir.net>
2017-09-20 14:27:31 +0300

    models dir

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramController extends Controller
 {
+    protected $telegram;
+
+    public function __construct(Telegram $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+    
     public function init()
     {
-        
+        $response = $this->telegram->getMe();
+
+        $botId = $response->getId();
+        $firstName = $response->getFirstName();
+        $username = $response->getUserName();
     }
 }
