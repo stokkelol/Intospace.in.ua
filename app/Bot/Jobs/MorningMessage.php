a377371468358de989204b08b057d020b8f9baed
Stokkelol <redwhite@bigmir.net>
2018-02-12 23:01:24 +0200

    Jobs

---
+++ app/Bot/Jobs/MorningMessage.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Bot\Jobs;
+
+use App\Models\TelegramUser;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+/**
+ * Class MorningMessage
+ *
+ * @package App\Bot\Jobs
+ */
+class MorningMessage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var TelegramUser
+     */
+    private $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param TelegramUser $user
+     */
+    public function __construct(TelegramUser $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        //
+    }
+}
