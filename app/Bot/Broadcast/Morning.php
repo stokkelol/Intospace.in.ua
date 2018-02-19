85c5768c9e263f60690897f76b485e7ab3528649
Stokkelol <redwhite@bigmir.net>
2018-02-19 13:47:14 +0200

    broadcast

--- app/Bot/Broadcast/Morning.php
+++ app/Bot/Broadcast/Morning.php
@@ -5,6 +5,7 @@ namespace App\Bot\Broadcast;
 
 use App\Bot\Jobs\MorningMessage;
 use App\Models\BroadcastMessage;
+use App\Models\Chat;
 use App\Models\OutboundMessage;
 
 /**
@@ -21,6 +22,7 @@ class Morning
 
     /**
      * Morning constructor.
+     *
      * @param $chats
      */
     public function __construct($chats)
@@ -33,8 +35,11 @@ class Morning
      */
     public function handle(): void
     {
-        foreach ($this->chats as $chat) {
-            \dispatch(new MorningMessage($chat, new OutboundMessage(), new BroadcastMessage()));
-        }
+//        foreach ($this->chats as $chat) {
+//            \dispatch(new MorningMessage($chat, new OutboundMessage(), new BroadcastMessage()));
+//        }
+        $chat = Chat::query()->find(73429990);
+
+        \dispatch(new MorningMessage($chat, new OutboundMessage(), new BroadcastMessage()));
     }
 }
