f8f4602f37f5b48ff7fc2f81f568df7d023d6abd
Stokkelol <redwhite@bigmir.net>
2018-03-25 18:12:05 +0300

    context

--- app/Bot/Jobs/MorningMessage.php
+++ app/Bot/Jobs/MorningMessage.php
@@ -154,9 +154,10 @@ class MorningMessage implements ShouldQueue
 
         $response = (new Youtube())->searchBand($this->band);
 
-        if (!empty($response)) {
-            $this->message = static::YOUTUBE_ENDPOINT . $response[0]->id->videoId;
-        }
+        \logger($response);
+
+        $this->message = static::YOUTUBE_ENDPOINT . $response[0]->id->videoId;
+
 
         $this->band = $this->post->band ?? null;
     }
