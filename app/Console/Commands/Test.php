7b0377b62331e51441010cd6bcc2aab2bc81a9d8
Stokkelol <redwhite@bigmir.net>
2018-03-27 22:42:03 +0300

    refactor statistics

---
+++ app/Console/Commands/Test.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Telegram\Bot\Api;
+
+/**
+ * Class Test
+ *
+ * @package App\Console\Commands
+ */
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'For telegram testing purposes';
+
+    /**
+     * Execute the console command.
+     *
+     * @param Api $api
+     * @return mixed
+     */
+    public function handle(Api $api)
+    {
+        $api->sendMessage([
+            'chat_id' => 73429990,
+            'text' => 'Hello!',
+            'parse_mode' => 'markdown',
+            'reply_markup' => \json_encode([
+                'inline_keyboard' => [
+                    'text' => 'text',
+                    'callback_data' => 'band_id:1'
+                ]
+            ])
+        ]);
+    }
+}
