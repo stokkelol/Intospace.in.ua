03d2536567d31852937a84f5f7d5e652e900e935
Stokkelol <redwhite@bigmir.net>
2018-03-28 21:35:52 +0300

    refactor statistics

--- app/Bot/ResponseMessages/CommandResponses/BaseCommand.php
+++ app/Bot/ResponseMessages/CommandResponses/BaseCommand.php
@@ -16,7 +16,7 @@ use App\Models\Video;
 abstract class BaseCommand
 {
     const POSTS_ENDPOINT = 'https://www.intospace.in.ua/posts/';
-    const YOUTUBE_ENDPOINT  = 'https://www.yputube.com/watch?v=';
+    const YOUTUBE_ENDPOINT  = 'https://www.youtube.com/watch?v=';
 
     /**
      * @var Post
