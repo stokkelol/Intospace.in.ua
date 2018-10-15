db6cc3117b197a9fb82965c7d3512a869aae4a12
redwhitelol <redwhite@bigmir.net>
2018-10-15 19:04:47 +0300

    callback data

--- app/Bot/Bot.php
+++ app/Bot/Bot.php
@@ -106,7 +106,7 @@ class Bot
     private function processCallbackData($request): void
     {
         $data = \json_decode($request['callback_query']['data'], true);
-        logger("data : " . $data['callback_query']['data']);
+        logger("data : " . $data['id']);
         $handler = Factory::build($data['callback_type'], $data);
         $handler->handle();
     }
