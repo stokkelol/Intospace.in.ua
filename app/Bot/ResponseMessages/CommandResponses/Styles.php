3a5cdbf1aeb6ae82f9430c0120c5af7fae345bbd
redwhitelol <redwhitepwnz@gmail.com>
2021-02-19 11:30:16 +0200

    Fix styles command when video not found

--- app/Bot/ResponseMessages/CommandResponses/Styles.php
+++ app/Bot/ResponseMessages/CommandResponses/Styles.php
@@ -37,21 +37,22 @@ class Styles extends BaseCommand implements Command
      */
     public function prepare(): array
     {
-        $tag = Tag::query()->where('tag', '=', $this->getTag())->with('bands')->first();
-
-        $this->band = $tag->bands->random();
-
-        $gatherer = new StatisticGatherer($this->user);
-        $gatherer->associateBandAndUser($this->band);
-        $gatherer->associateTagAndUser($this->band);
-
-        $searcher = new Youtube();
-        $result = $searcher->searchBand($this->band);
-
-        $this->album = $searcher->getAlbum();
-        $this->track = $searcher->getTrack();
+        do {
+            $this->searchBand();
+            $searcher = new Youtube();
+            $result = $searcher->searchBand($this->band);
+        } while (!\is_array($result));
 
         return [static::YOUTUBE_ENDPOINT . $this->getVideoId($result)];
+    }
+
+    /**
+     * @return array
+     */
+    public function searchBand(): void
+    {
+        $tag = Tag::query()->where('tag', '=', $this->getTag())->with('bands')->first();
+        $this->band = $tag->bands->random();
     }
 
     /**
